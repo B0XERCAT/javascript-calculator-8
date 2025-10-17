c5bd0d3543a8cc975e2f2be7fa8bb2b2c30c914d
B0XERCAT <seojin3154@naver.com>
2025-10-17 20:12:59 +0900

    fix: integrate prettier with eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
     ignorePatterns: ["package*", ".npmrc", "*.md", ".*", "__tests__"],
     extends: [
       "eslint:recommended",
-      "airbnb-base"
+      "airbnb-base",
+      "prettier"
     ],
 };
