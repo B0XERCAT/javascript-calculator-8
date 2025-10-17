a14a3283246d7e5588ba93fcdfb6e879436a7a43
B0XERCAT <seojin3154@naver.com>
2025-10-17 20:24:40 +0900

    feat: handle user input and detect custom delimiter by regex

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # javascript-calculator-precourse
 - [x]  Airbnb ESLint 설정
 - [x]  Airbnb Prettier 설정
-- [ ]  사용자 입력 처리 기능
+- [x]  사용자 입력 처리 기능
 - [ ]  기본 구분자(쉼표, 콜론)로 숫자 추출 기능
 - [ ]  커스텀 구분자 파싱 및 숫자 추출 기능
 - [ ]  숫자 배열의 합산 및 출력 기능
--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console } from '@woowacourse/mission-utils';
+
+const REGEX = /^\/\/([^0-9])\\n/;
+
 class App {
-  async run() {}
+  async run() {
+    Console.readLine('덧셈할 문자열을 입력해 주세요.\n', (userInput) => {
+      const match = userInput.match(REGEX)
+      
+      if (match) {
+        const customDelimiter = match[1];
+        // extract numbers by delimiters
+      } else {
+        // extract numbers by delimiter
+      }
+
+      // print sum of numbers
+    });
+  }
 }
 
 export default App;
