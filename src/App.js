f739508e276e4feca57907262635d14d9e7405b8
B0XERCAT <seojin3154@naver.com>
2025-10-18 16:42:13 +0900

    feat: impl extract numbers using delimiter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import { Console } from '@woowacourse/mission-utils';
 
-const REGEX = /^\/\/([^0-9])\\n/;
+const CUSTOM_DELIMITER_EXTRACTOR = /^\/\/([^0-9])\\n/;
+const DEFAULT_DELIMITER = /,|:/;
+
+function extractNumbers(inputString, delimiter) {
+  const splitValues = inputString.split(delimiter);
+  const parsedNumbers = splitValues.map(Number);
+  return parsedNumbers;
+}
 
 class App {
   async run() {
     Console.readLine('덧셈할 문자열을 입력해 주세요.\n', (userInput) => {
-      const match = userInput.match(REGEX)
-      
-      if (match) {
-        const customDelimiter = match[1];
-        // extract numbers by delimiters
+      const customDelimiterMatch = userInput.match(CUSTOM_DELIMITER_EXTRACTOR);
+      let parsedNumbers;
+
+      if (customDelimiterMatch) {
+        const numberSection = userInput.split('\\n')[1];
+        const customDelimiter = customDelimiterMatch[1];
+        parsedNumbers = extractNumbers(numberSection, customDelimiter);
       } else {
-        // extract numbers by delimiter
+        parsedNumbers = extractNumbers(userInput, DEFAULT_DELIMITER);
       }
 
       // print sum of numbers
